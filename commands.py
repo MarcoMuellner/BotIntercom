db5e08afe5db0c25c97d3060c26b32ec0782ee96
mm7490 <muellnermarco@gmail.com>
2019-07-18 20:32:58 +0200

    Auto stash before merge of "master" and "origin/master"

--- commands.py
+++ commands.py
@@ -5,12 +5,12 @@ import json
 from BotIntercom.settings import port,qotd_post,punisher_post
 
 def send_qotd_request(position_list : df, g_id : int, g_name : str):
-    if 'player_d_id' not in df or 'player_position' not in df:
+    if 'player_d_id' not in position_list or 'player_position' not in position_list:
         raise ValueError("player_d_id and player_position need to exist in Dataframe.")
     send_dict = {'result' : []}
     for p in position_list.iterrows():
         p = p[1]
-        send_dict['result'].append([p.player_d_id,p.player_position])
+        send_dict['result'].append([int(p.player_d_id),int(p.player_position)])
     send_dict['guild_id'] = g_id
     send_dict['guild_name'] = g_name
     send_dict['post_source'] = qotd_post
