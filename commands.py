a75144125ee355018d19b024d6e736946a8d2459
mm7490 <muellnermarco@gmail.com>
2019-07-19 16:55:01 +0200

    Added voter list for QOTD

--- commands.py
+++ commands.py
@@ -1,10 +1,11 @@
 from pandas import DataFrame as df
 import requests
 import json
+from typing import List
 
 from BotIntercom.settings import port,qotd_post,punisher_post
 
-def send_qotd_request(position_list : df,question_user_id : int, g_id : int, g_name : str):
+def send_qotd_request(position_list : df,question_user_id : int, g_id : int, g_name : str,total_react_list : List[int]):
     if 'player_d_id' not in position_list or 'player_position' not in position_list:
         raise ValueError("player_d_id and player_position need to exist in Dataframe.")
     send_dict = {'result' : []}
@@ -15,6 +16,7 @@ def send_qotd_request(position_list : df,question_user_id : int, g_id : int, g_n
     send_dict['guild_name'] = g_name
     send_dict['post_source'] = qotd_post
     send_dict['question_player_id'] = question_user_id
+    send_dict['react_list'] = total_react_list
     r = requests.post(f'http://localhost:{port}',data=json.dumps(send_dict))
 
 def send_punisher_request(player_id : int, punisher_points : int , g_id : int, g_name : str,undo=False):
